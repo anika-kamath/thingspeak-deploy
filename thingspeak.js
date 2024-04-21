d4cba31c2310cdc2d8f3cf4e739d84ae8233e6f6
Anika Kamath <87904385+anika-kamath@users.noreply.github.com>
2024-04-21 11:25:59 +0530

    Update thingspeak.js

--- thingspeak.js
+++ thingspeak.js
@@ -1,13 +1,33 @@
-function getData() {
-  const channelId = "2191567"; // Replace with your ThingSpeak channel ID
-  const apiReadKey = "BU9640W3HOS6KPQT"; // Replace with your ThingSpeak Read API Key
-  const url = `https://api.thingspeak.com/channels/${channelId}/fields/1/last.json?api_key=${apiReadKey}`;
+const uploadInput = document.getElementById('upload1');
+const outputImage = document.getElementById('output-image');
+const submitButton = document.getElementById('submit-button');
+const channelId = "2191567"; // Replace with your ThingSpeak channel ID
+const apiReadKey = "BU9640W3HOS6KPQT"; // Replace with your ThingSpeak Read API Key
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("temperature").textContent = `Temperature: ${data.field1} °C`;
-      document.getElementById("humidity").textContent = `Humidity: ${data.field1} %`;
-    })
-    .catch(error => console.error(error));
-}
+submitButton.addEventListener('click', () => {
+  const uploadedFile = uploadInput.files[0];
+  if (uploadedFile) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      outputImage.src = event.target.result;
+    };
+    reader.readAsDataURL(uploadedFile);
+  } else {
+    // Handle case where no file is uploaded
+    alert('Please select an image to upload.');
+  }
+});
+
+
+// function getData() {
+//   
+//   const url = `https://api.thingspeak.com/channels/${channelId}/fields/1/last.json?api_key=${apiReadKey}`;
+
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//       document.getElementById("temperature").textContent = `Temperature: ${data.field1} °C`;
+//       document.getElementById("humidity").textContent = `Humidity: ${data.field1} %`;
+//     })
+//     .catch(error => console.error(error));
+// }
